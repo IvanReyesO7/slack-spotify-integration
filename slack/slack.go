e54e00e094a0df5d2afb1523e08193d06c3ddc86
Ivan Reyes <ivanwilf@hotmail.com>
2023-01-22 12:16:10 +0900

    deleted slack token

--- slack/slack.go
+++ slack/slack.go
@@ -11,10 +11,10 @@ import (
 // }
 
 func Ping(channel string, thread_ts string) {
-	// api := slack.New("xoxb-2882488176691-4678049065891-d9eT4MiNpUPpoUCLo41YNNs7")
+	// api := slack.New("")
 	// If you set debugging, it will log all requests to the console
 	// Useful when encountering issues
-	api := slack.New("xoxb-2882488176691-4678049065891-d9eT4MiNpUPpoUCLo41YNNs7", slack.OptionDebug(true))
+	api := slack.New("...", slack.OptionDebug(true))
 	_, _, err := api.PostMessage(channel, slack.MsgOptionTS(thread_ts), slack.MsgOptionText("Got it!", true))
 
 	if err != nil {
