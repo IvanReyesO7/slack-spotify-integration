9c7449d38c9d9359c96b312984653b1bb0555840
Ivan Reyes <ivanwilf@hotmail.com>
2023-01-22 16:30:51 +0900

    Added new attribute to Spotify.Song structure

--- slack/slack.go
+++ slack/slack.go
@@ -12,14 +12,19 @@ import (
 func SendTracks(channel string, thread_ts string, tracks []Spotify.Song) {
 	fmt.Println(tracks)
 	api := slack.New(os.Getenv("SLACK_TOKEN"), slack.OptionDebug(true))
-	_, _, err := api.PostMessage(channel, slack.MsgOptionTS(thread_ts), slack.MsgOptionText("Got it!", true))
 
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
+	for _, track := range tracks {
+		json := formatMessage(track)
+		fmt.Println(json)
+		_, _, err := api.PostMessage(channel, slack.MsgOptionTS(thread_ts), slack.MsgOptionText(track.Title, true))
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+
 	}
 }
 
-func formatMessage(tracks []Spotify.Song) {
-
+func formatMessage(track Spotify.Song) string {
+	return ""
 }
--- spotify/spotify.go
+++ spotify/spotify.go
@@ -14,6 +14,7 @@ import (
 )
 
 type Song struct {
+	Id       int
 	Title    string
 	Album    string
 	Artist   string
