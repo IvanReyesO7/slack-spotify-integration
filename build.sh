ae510ffbc01a5d4867fb1950282589cff266116f
Ivan Reyes <ivanwilf@hotmail.com>
2023-01-23 10:02:19 +0900

    Added build.sh for an easier project set up

---
+++ build.sh
@@ -0,0 +1,6 @@
+go mod init slack-spotify-integration
+go get github.com/gin-gonic/gin
+go get github.com/slack-go/slack
+go get github.com/zmb3/spotify/v2
+go get github.com/zmb3/spotify/v2/auth
+go get golang.org/x/oauth2/clientcredentials
