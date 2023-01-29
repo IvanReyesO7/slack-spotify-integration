4ccd108b8a6efa04bdfddb74bf8a5823de85caa9
Ivan Reyes <ivanwilf@hotmail.com>
2023-01-29 16:13:38 +0900

    Adding pending packages

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ go get github.com/zmb3/spotify/v2/auth
 go get golang.org/x/oauth2/clientcredentials
 go get github.com/tidwall/gjson
 go get github.com/joho/godotenv
+go get github.com/mattn/go-isatty@v0.0.16
+go get github.com/go-playground/validator/v10@v10.11.1
+go get golang.org/x/net/idna@v0.5.0
